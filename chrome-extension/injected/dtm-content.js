9c5e26cd6b749df2c86dc898d2286e5afc30a27c
Gilmore Davidson <gilmoreorless@gmail.com>
2014-03-04 22:48:10 +1100

    Work with defined transform-origin, not the calculated-as-pixels value

--- chrome-extension/injected/dtm-content.js
+++ chrome-extension/injected/dtm-content.js
@@ -4,38 +4,54 @@ function A(obj) {
 	return Array.prototype.slice.call(obj || []);
 }
 function AtoJ(obj) {
-	return A(obj).map(toJSON);
+	return A(obj).map(JSONable);
 }
 
 var JSONConvertors = {
+	Object: function (obj) {
+		var clone = {};
+		for (var key in obj) {
+			clone[key] = JSONable(obj[key]);
+		}
+		return clone;
+	},
+
 	CSSStyleDeclaration: function getStyleProps(style) {
-		var styles = {};
-		A(style).concat('cssText').forEach(function (prop) {
-			styles[prop] = style[prop];
-		});
-		return styles;
+		var copy = {};
+		for (var prop in style) {
+			if (prop === 'length' || (style[prop] && typeof style[prop] === 'string')) {
+				copy[prop] = style[prop];
+			}
+		}
+		return copy;
 	},
 	CSSStyleRule: function getRuleProps(rule) {
 		return {
 			cssText: rule.cssText,
 			selectorText: rule.selectorText,
-			style: toJSON(rule.style)
+			style: JSONable(rule.style)
 		};
 	},
 	CSSRuleList: AtoJ
 };
 
-function toJSON(obj) {
+function JSONable(obj) {
+	if (obj == null) {
+		return obj;
+	}
 	var con = obj.constructor.name;
 	if (JSONConvertors[con]) {
 		return JSONConvertors[con](obj);
+	}
+	if (Array.isArray(obj)) {
+		return obj.map(JSONable);
 	}
 	return obj;
 }
 
 function getStylesAndRules(elem) {
 	var computed = getComputedStyle(elem);
-	var rules = getMatchedCSSRules(elem);
+	var rules = getMatchedCSSRules(elem) || [];
 	var inlineStyle = elem.style;
 
 	console.log({
@@ -43,9 +59,9 @@ function getStylesAndRules(elem) {
 		rules: rules,
 		inlineStyle: inlineStyle
 	});
-	return toJSON({
-		computed: toJSON(computed),
-		rules: toJSON(rules),
-		inlineStyle: toJSON(inlineStyle)
+	return JSONable({
+		computed: computed,
+		rules: rules,
+		inlineStyle: inlineStyle
 	});
 }
--- chrome-extension/tools/transform/sidebar.js
+++ chrome-extension/tools/transform/sidebar.js
@@ -1,9 +1,28 @@
+function getAppliedValue(defs, prop, defaultValue) {
+	// TODO: This is really simple and naive for testing - make it work properly with specificity
+	var applied = defs.inlineStyle[prop];
+	if (!applied) {
+		defs.rules.forEach(function (rule) {
+			if (rule.style[prop] !== undefined) {
+				applied = rule.style[prop];
+			}
+		});
+	}
+	if (applied === undefined) {
+		applied = defaultValue;
+	}
+	return applied;
+}
+
 function update() {
 	chrome.devtools.inspectedWindow.eval('getStylesAndRules($0)', {useContentScriptContext: true}, function (result) {
-		document.getElementById('holder').innerHTML = 'transform: ' + result.computed['-webkit-transform'] +
-			'<br/>transform-origin: ' + result.computed['-webkit-transform-origin'];
-		document.querySelector('.trans-elem').style.webkitTransform = result.computed['-webkit-transform'];
-		document.querySelector('.trans-elem').style.webkitTransformOrigin = result.computed['-webkit-transform-origin'];
+		var transform = getAppliedValue(result, 'webkitTransform', 'none');
+		var origin = getAppliedValue(result, 'webkitTransformOrigin', '50% 50%');
+
+		document.getElementById('holder').innerHTML = 'transform: ' + transform +
+			'<br/>transform-origin: ' + origin;
+		document.querySelector('.trans-elem').style.webkitTransform = transform;
+		document.querySelector('.trans-elem').style.webkitTransformOrigin = origin;
 	});
 }
 
