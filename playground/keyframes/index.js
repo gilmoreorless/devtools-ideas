145e0bca0e4d50498e91fbc97ad593d069716f0f
Gilmore Davidson <gilmoreorless@gmail.com>
2014-03-07 08:41:58 +1100

    Mucking around with keyframes

---
+++ playground/keyframes/index.js
@@ -0,0 +1,51 @@
+(function () {
+
+    var dom = {};
+    var domIds = ['input-css', 'parse-submit', 'parse-result', 'animate-me', 'main-container'];
+    domIds.forEach(function (id) {
+        newId = id.replace(/-(\w)/g, function (_, c) { return c.toUpperCase(); });
+        dom[newId] = document.getElementById(id);
+    });
+
+    function parse(str) {
+        var parsed;
+        var output = dom.parseResult;
+        output.textContent = '';
+        try {
+            parsed = CSSParse(str);
+            // output.textContent = JSON.stringify(parsed, null, 2);
+            animate(str, parsed);
+        } catch (e) {
+            output.textContent = e;
+        }
+    }
+
+    function animate(str, ast) {
+        var animation = ast.stylesheet.rules.filter(function (rule) {
+            return rule.type === 'keyframes';
+        })[0];
+        if (!animation) return;
+
+        var name = animation.name;
+        var sheet = document.createElement('style');
+        sheet.textContent = str;
+        document.head.appendChild(sheet);
+
+        function listener() {
+            dom.animateMe.removeEventListener('animationend', listener, false);
+            dom.animateMe.removeEventListener('webkitAnimationEnd', listener, false);
+            dom.animateMe.style.removeProperty('animation');
+            dom.animateMe.style.removeProperty('-webkit-animation');
+            document.head.removeChild(sheet);
+        }
+        dom.animateMe.addEventListener('animationend', listener, false);
+        dom.animateMe.addEventListener('webkitAnimationEnd', listener, false);
+        dom.animateMe.style.animationName = name;
+        dom.animateMe.style.webkitAnimationName = name;
+    }
+
+    dom.parseSubmit.addEventListener('click', function () {
+        parse(dom.inputCss.value);
+    }, false);
+
+})();
